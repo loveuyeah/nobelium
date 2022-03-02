bbe82db7f3cec842fe3a755074edd1c7c513c8ec
loveuyeah <44426243+loveuyeah@users.noreply.github.com>
2022-03-03 07:31:30 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,22 +2,22 @@ const BLOG = {
   title: 'ming',
   author: 'MK',
   email: '',
-  link: 'https://loveuyeah.vercel.app',
+  link: '',
   description: '',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'ja-JP', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'dark', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#282828', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 50,
+  postsPerPage: 500,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://loveuyeah.Github.io',
+  socialLink: '',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
